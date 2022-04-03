0691e4c7c03a7c95380b8ec685db98b543349673
khadada <lkhadada@gmail.com>
2022-04-03 16:29:04 +0100

    Now I will start work with initialize_ui method

--- richNotepad.py
+++ richNotepad.py
@@ -4,3 +4,15 @@ import sys
 from PyQt5.QtWidgets import (QApplication, QMainWindow, QAction, QMessageBox,QTextEdit,QFileDialog,QInputDialog,QFontDialog,QColorDialog)
 from PyQt5.QtGui import QIcon, QColor, QTextCursor
 from PyQt5.QtCore import Qt
+
+# Create Own Notepad:
+class RichNotepad(QMainWindow):
+    def __init__(self):
+        super().__init__()
+        self.initialize_ui()
+        self.show()
+    
+    def initialize_ui(self):
+        """
+        Initialize the window and display its contents to the screen
+        """
