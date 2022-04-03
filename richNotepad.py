bd562bf0c5c623909cf52fa638978d509eba7f30
khadada <lkhadada@gmail.com>
2022-04-03 16:18:07 +0100

    I imported the necessary modules and classes to work wit

---
+++ richNotepad.py
@@ -0,0 +1,6 @@
+# Rich Text Notepad GUI 
+# Importing necessary modules and classes:
+import sys
+from PyQt5.QtWidgets import (QApplication, QMainWindow, QAction, QMessageBox,QTextEdit,QFileDialog,QInputDialog,QFontDialog,QColorDialog)
+from PyQt5.QtGui import QIcon, QColor, QTextCursor
+from PyQt5.QtCore import Qt
